f38893327b1439e046092f55021ae7c414ccfe0b
Amr Elsawalhi <amr.elsawalhi.business@gmail.com>
2025-06-28 22:39:04 +0300

    added a hard-coded useragent

--- Scraper.py
+++ Scraper.py
@@ -27,7 +27,7 @@ def scrape_jumia(product_name, max_results=10):
 
     # Rotate user-agent
     headers = {
-        "User-Agent": ua.random,
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
         "Accept-Language": "en-US,en;q=0.9"
     }
 
